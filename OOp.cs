19f92f846c07c0f4616aeec8002be73af2a44381
Tanzeela Fatima <kashmirirajpoot128@gmail.com>
2025-01-31 09:37:41 +0500

    Add more files to complete OOP concept

---
+++ OOp.cs
@@ -0,0 +1,17 @@
+//Basic Class
+
+public class Student{
+    public int Id{get;set;}
+    public string Name{get;set;}
+    public void SaveStudent()
+    {
+
+
+    }
+    //Public how function is public next  data type of function
+    public void ShowFullName()
+    {
+
+
+    }
+}
---
+++ Using class.cs
@@ -0,0 +1,3 @@
+Student Arooj=new Student();  //object of class
+Arooj.Id =1;
+Arooj.Name="Arooj";
